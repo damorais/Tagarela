bbd24179d78ad5cee90bbb381159020000a7571c
Daniel Morais <damorais@gmail.com>
2021-01-12 11:20:34 -0300

    Commit inicial

---
+++ Data/TagarelaContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Tagarela.Models;
+
+namespace Tagarela.Data
+{
+    public class TagarelaContext : DbContext
+    {
+        public TagarelaContext(DbContextOptions<TagarelaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mensagem> Mensagem { get; set; }
+    }
+}
